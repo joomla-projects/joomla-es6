40c4e9e5eb064a72c0019d6ae11269c0c33b8486
astridx <info@astrid-guenther.de>
2018-02-24 20:24:34 +0100

    delete unused code

--- media/com_plugins/js/edit.es6.js
+++
@@ -1,9 +0,0 @@
-/**
- * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
-Joomla.submitbutton = (task) => {
-  if (task === 'plugin.cancel' || document.formvalidator.isValid(document.getElementById('style-form'))) {
-    Joomla.submitform(task, document.getElementById('style-form'));
-  }
-};
