ad114f69b49df707d78063dcb11cb1173409d80a
Dimitris Grammatikogiannis <d.grammatiko@gmail.com>
2018-02-27 09:58:59 +0100

    field category

--- administrator/components/com_banners/tmpl/banner/edit.php
+++ administrator/components/com_banners/tmpl/banner/edit.php
@@ -13,7 +13,6 @@ JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
 
 JHtml::_('jquery.framework');
 JHtml::_('behavior.formvalidator');
-JHtml::_('formbehavior.chosen', '#jform_catid', null, array('disable_search_threshold' => 0 ));
 
 JHtml::_('script', 'com_banners/admin-banner-edit.min.js', array('version' => 'auto', 'relative' => true));
 ?>
---
+++ build/webcomponents/js/field-category/field-category.js
@@ -0,0 +1,35 @@
+; customElements.define('joomla-field-category', class extends HTMLElement {
+  constructor() {
+    super();
+
+    this.element = '';
+    Joomla.loadingLayer('load', document.body);
+  }
+
+  connectedCallback() {
+    // Check if custom fields are enabled
+    if (this.getAttribute('custom-fields-enabled') !== 'true') {
+      return;
+    }
+
+    this.element = this.querySelector('select');
+    this.categoryHasChanged = this.categoryHasChanged.bind(this);
+
+    if (!this.element.value !== this.getAttribute('custom-fields-cat-id')) {
+      this.element.value = this.getAttribute('custom-fields-cat-id');
+    }
+
+    this.element.addEventListener('change', this.categoryHasChanged);
+  }
+
+  categoryHasChanged() {
+    if (this.element.value === parseInt(this.element.parentNode.getAttribute('custom-fields-cat-id'))) {
+      return;
+    }
+
+    Joomla.loadingLayer('show', document.body);
+
+    document.querySelector('input[name=task]').value = this.element.parentNode.getAttribute('custom-fields-section') + '.reload';
+    this.element.form.submit();
+  }
+});
