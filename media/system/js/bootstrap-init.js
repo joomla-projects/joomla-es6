396cda0e08b23b9b38332742046016672acd04e4
Dimitris Grammatikogiannis <d.grammatiko@gmail.com>
2018-02-27 11:59:47 +0100

    boostrap init

--- media/system/js/bootstrap-init.js
+++ media/system/js/bootstrap-init.js
@@ -1,142 +1,146 @@
-;(function() {
-	"use strict";
+((document, getOptions, $) => {
+  "use strict";
 
-	jQuery(document).ready(function($) {
+  if (typeof getOptions !== 'function') {
+    throw new Error('Joomla API is not properly initialised');
+  }
 
-		// Initialize some variables
-		var accordion = Joomla.getOptions('bootstrap.accordion'),
-		    alert = Joomla.getOptions('bootstrap.alert'),
-		    button = Joomla.getOptions('bootstrap.button'),
-		    carousel = Joomla.getOptions('bootstrap.carousel'),
-		    dropdown = Joomla.getOptions('bootstrap.dropdown'),
-		    modal = $('.joomla-modal'),
-		    popover = Joomla.getOptions('bootstrap.popover'),
-		    scrollspy = Joomla.getOptions('bootstrap.scrollspy'),
-		    tabs = Joomla.getOptions('bootstrap.tabs'),
-		    tooltip = Joomla.getOptions('bootstrap.tooltip');
+  $(document).ready(($) => {
 
-		/** Accordion **/
-		if (accordion) {
-			$.each(accordion, function(index, value) {
-				$('#' + index).collapse(
-					{
-						parent:value.parent,
-						toggle:value.toggle
-					}
-				).on("show", new Function(value.onShow)())
-					.on("shown", new Function(value.onShown)())
-					.on("hideme", new Function(value.onHide)())
-					.on("hidden", new Function(value.onHidden)());
-			});
-		}
+    // Initialize some variables
+    var accordion = getOptions('bootstrap.accordion'),
+      alert = getOptions('bootstrap.alert'),
+      button = getOptions('bootstrap.button'),
+      carousel = getOptions('bootstrap.carousel'),
+      dropdown = getOptions('bootstrap.dropdown'),
+      modal = $('.joomla-modal'),
+      popover = getOptions('bootstrap.popover'),
+      scrollspy = getOptions('bootstrap.scrollspy'),
+      tabs = getOptions('bootstrap.tabs'),
+      tooltip = getOptions('bootstrap.tooltip');
 
-		/** Alert **/
-		if (alert) {
-			$.each(alert, function(index, value) {
-				$('#' + index).alert();
-			});
-		}
+    /** Accordion **/
+    if (accordion) {
+      $.each(accordion, (index, value) => {
+        $('#' + index).collapse(
+          {
+            parent: value.parent,
+            toggle: value.toggle
+          }
+        ).on("show", new Function(value.onShow)())
+          .on("shown", new Function(value.onShown)())
+          .on("hideme", new Function(value.onHide)())
+          .on("hidden", new Function(value.onHidden)());
+      });
+    }
 
-		/** Button **/
-		if (button) {
-			$.each(button, function(index, value) {
-				$('#' + index).button();
-			});
-		}
+    /** Alert **/
+    if (alert) {
+      $.each(alert, (index, value) => {
+        $('#' + index).alert();
+      });
+    }
 
-		/** Carousel **/
-		if (carousel) {
-			$.each(carousel, function(index, value) {
-				$('#' + index).carousel(
-					{
-						interval: value.interval ? value.interval : 5000,
-						pause: value.pause ? value.pause : 'hover'
-					}
-				);
-			});
-		}
+    /** Button **/
+    if (button) {
+      $.each(button, (index, value) => {
+        $('#' + index).button();
+      });
+    }
 
-		/** Dropdown menu **/
-		if (dropdown) {
-			$.each(dropdown, function(index, value) {
-				$('#' + index).dropdown();
-			});
-		}
+    /** Carousel **/
+    if (carousel) {
+      $.each(carousel, (index, value) => {
+        $('#' + index).carousel(
+          {
+            interval: value.interval ? value.interval : 5000,
+            pause: value.pause ? value.pause : 'hover'
+          }
+        );
+      });
+    }
 
-		/** Modals **/
-		if (modal.length) {
-			$.each($('.joomla-modal'), function() {
-				var $self = $(this);
-				$self.on('show.bs.modal', function() {
-					if ($self.data('url')) {
-						var modalBody = $self.find('.modal-body');
-						modalBody.find('iframe').remove();
-						modalBody.prepend($self.data('iframe'));
-					}
-				}).on('shown.bs.modal', function() {
-					var modalHeight = $('div.modal:visible').outerHeight(true),
-					    modalHeaderHeight = $('div.modal-header:visible').outerHeight(true),
-					    modalBodyHeightOuter = $('div.modal-body:visible').outerHeight(true),
-					    modalBodyHeight = $('div.modal-body:visible').height(),
-					    modalFooterHeight = $('div.modal-footer:visible').outerHeight(true),
-					    padding = $self.offsetTop,
-					    maxModalHeight = ($(window).height()-(padding*2)),
-					    modalBodyPadding = (modalBodyHeightOuter-modalBodyHeight),
-					    maxModalBodyHeight = maxModalHeight-(modalHeaderHeight+modalFooterHeight+modalBodyPadding);
-					if ($self.data('url')) {
-						var iframeHeight = $('.iframe').height();
-						if (iframeHeight > maxModalBodyHeight){
-							$('.modal-body').css({'max-height': maxModalBodyHeight, 'overflow-y': 'auto'});
-							$('.iframe').css('max-height', maxModalBodyHeight-modalBodyPadding);
-						}
-					}
-				}).on('hide.bs.modal', function () {
-					$('.modal-body').css({'max-height': 'initial', 'overflow-y': 'initial'});
-					$('.modalTooltip').tooltip('dispose');
-				});
-			});
-		}
+    /** Dropdown menu **/
+    if (dropdown) {
+      $.each(dropdown, (index, value) => {
+        $('#' + index).dropdown();
+      });
+    }
 
-		/** Popover **/
-		if (popover) {
-			$.each(popover, function(index, value) {
-				value.constraints = [value.constraints];
-				$(index).popover(value);
-			});
-		}
+    /** Modals **/
+    if (modal.length) {
+      $.each($('.joomla-modal'), () => {
+        var $self = $(this);
+        $self.on('show.bs.modal', () => {
+          if ($self.data('url')) {
+            var modalBody = $self.find('.modal-body');
+            modalBody.find('iframe').remove();
+            modalBody.prepend($self.data('iframe'));
+          }
+        }).on('shown.bs.modal', () => {
+          var modalHeight = $('div.modal:visible').outerHeight(true),
+            modalHeaderHeight = $('div.modal-header:visible').outerHeight(true),
+            modalBodyHeightOuter = $('div.modal-body:visible').outerHeight(true),
+            modalBodyHeight = $('div.modal-body:visible').height(),
+            modalFooterHeight = $('div.modal-footer:visible').outerHeight(true),
+            padding = $self.offsetTop,
+            maxModalHeight = ($(window).height() - (padding * 2)),
+            modalBodyPadding = (modalBodyHeightOuter - modalBodyHeight),
+            maxModalBodyHeight = maxModalHeight - (modalHeaderHeight + modalFooterHeight + modalBodyPadding);
+          if ($self.data('url')) {
+            var iframeHeight = $('.iframe').height();
+            if (iframeHeight > maxModalBodyHeight) {
+              $('.modal-body').css({ 'max-height': maxModalBodyHeight, 'overflow-y': 'auto' });
+              $('.iframe').css('max-height', maxModalBodyHeight - modalBodyPadding);
+            }
+          }
+          }).on('hide.bs.modal', () => {
+          $('.modal-body').css({ 'max-height': 'initial', 'overflow-y': 'initial' });
+          $('.modalTooltip').tooltip('dispose');
+        });
+      });
+    }
 
-		/** Scrollspy **/
-		if (scrollspy) {
-			$.each(scrollspy, function(index, value) {
-				$('#' + index).scrollspy(value);
-			});
-		}
+    /** Popover **/
+    if (popover) {
+      $.each(popover, (index, value) => {
+        value.constraints = [value.constraints];
+        $(index).popover(value);
+      });
+    }
 
-		/** Tabs **/
-		if (tabs) {
-			$.each(tabs, function(index, value) {
+    /** Scrollspy **/
+    if (scrollspy) {
+      $.each(scrollspy, (index, value) => {
+        $('#' + index).scrollspy(value);
+      });
+    }
 
-				$.each($('#' + index + 'Content').find('.tab-pane'), function(i, v) {
-					if ($(v).data('node')) {
-						var attribs = $(v).data('node').split('['),
-						    classLink = (attribs[0] != '') ? 'class="nav-link ' + attribs[0] + '"' : 'class="nav-link"';
+    /** Tabs **/
+    if (tabs) {
+      $.each(tabs, (index, value) => {
 
-						$('#' + index + 'Tabs').append('<li class="nav-item"><a ' + classLink + ' href="#' + attribs[1] + '" data-toggle="tab">' + attribs[2] + '</a></li>');
-					}
-				});
-			});
-		}
+        $.each($('#' + index + 'Content').find('.tab-pane'), (i, v) => {
+          if ($(v).data('node')) {
+            var attribs = $(v).data('node').split('['),
+              classLink = (attribs[0] != '') ? 'class="nav-link ' + attribs[0] + '"' : 'class="nav-link"';
 
-		/** Tooltip **/
-		if (tooltip) {
-			$.each(tooltip, function(index, value) {
-				value.constraints = [value.constraints];
-				$(index).tooltip(value)
-					.on("show.bs.tooltip", new Function(value.onShow)())
-					.on("shown.bs.tooltip", new Function(value.onShown)())
-					.on("hide.bs.tooltip", new Function(value.onHide)())
-					.on("hidden.bs.tooltip", new Function(value.onHidden)());
-			});
-		}
-	});
-})();
+            $('#' + index + 'Tabs').append('<li class="nav-item"><a ' + classLink + ' href="#' + attribs[1] + '" data-toggle="tab">' + attribs[2] + '</a></li>');
+          }
+        });
+      });
+    }
+
+    /** Tooltip **/
+    if (tooltip) {
+      $.each(tooltip, (index, value) => {
+        value.constraints = [value.constraints];
+        $(index).tooltip(value)
+          .on("show.bs.tooltip", new Function(value.onShow)())
+          .on("shown.bs.tooltip", new Function(value.onShown)())
+          .on("hide.bs.tooltip", new Function(value.onHide)())
+          .on("hidden.bs.tooltip", new Function(value.onHidden)());
+      });
+    }
+  });
+})(document, Joomla.getOptions, jQuery);
