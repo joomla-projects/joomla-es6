5787e2df2d56bc225121819bfc7f0ff006e58954
Anurag <anuragvns1111@gmail.com>
2018-02-27 11:55:17 +0530

    com_user es6

--- media/com_users/js/admin-users-user.js
+++ media/com_users/js/admin-users-user.js
@@ -1,37 +1,33 @@
 /**
+* PLEASE DO NOT MODIFY THIS FILE. WORK ON THE ES6 VERSION.
+* OTHERWISE YOUR CHANGES WILL BE REPLACED ON THE NEXT BUILD.
+**/
+
+/**
  * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 Joomla = window.Joomla || {};
 
-(function(Joomla) {
-	'use strict';
+(function (Joomla) {
+  'use strict';
 
-	document.addEventListener('DOMContentLoaded', function() {
-
-		Joomla.twoFactorMethodChange = function(e) {
-			var method = document.getElementById('jform_twofactor_method');
-
-			if (method) {
-				var selectedPane   = 'com_users_twofactor_' + method.value,
-				    twoFactorForms = document.querySelectorAll('#com_users_twofactor_forms_container > div');
-
-				for (var i = 0; i < twoFactorForms.length; i++) {
-					var id = twoFactorForms[i].id;
-
-					if (id != selectedPane)
-					{
-						document.getElementById(id).style.display = 'none';
-					}
-					else
-					{
-						document.getElementById(id).style.display = 'block';
-					}
-				}
-			}
-		};
-
-	});
-
+  document.addEventListener('DOMContentLoaded', function () {
+    Joomla.twoFactorMethodChange = function () {
+      var method = document.getElementById('jform_twofactor_method');
+      if (method) {
+        var selectedPane = 'com_users_twofactor_' + method.value;
+        var twoFactorForms = document.querySelectorAll('#com_users_twofactor_forms_container > div');
+        twoFactorForms.forEach(function (value) {
+          var id = value.id;
+          if (id !== selectedPane) {
+            document.getElementById(id).style.display = 'none';
+          } else {
+            document.getElementById(id).style.display = 'block';
+          }
+        });
+      }
+    };
+  });
 })(Joomla);
