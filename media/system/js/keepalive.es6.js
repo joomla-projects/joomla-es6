068d59c01838181e9566bde6066b953b51d53af2
Dimitris Grammatikogiannis <d.grammatiko@gmail.com>
2018-03-12 15:11:19 +0200

    dog

--- media/system/js/keepalive.es6.js
+++ media/system/js/keepalive.es6.js
@@ -11,25 +11,28 @@
  * @package  Joomla
  * @since    3.7.0
  */
-((window, document, getOptions, Request) => {
+((window, document, getOptions, request) => {
   'use strict';
+
   document.addEventListener('DOMContentLoaded', () => {
     const keepaliveOptions = getOptions('system.keepalive');
-    let keepaliveUri = keepaliveOptions && keepaliveOptions.uri ? keepaliveOptions.uri.replace(/&amp;/g, '&') : '';
-    const keepaliveInterval = keepaliveOptions && keepaliveOptions.interval ? keepaliveOptions.interval : 45 * 1000;
+    let keepaliveUri = keepaliveOptions && keepaliveOptions.uri
+      ? keepaliveOptions.uri.replace(/&amp;/g, '&') : '';
+    const keepaliveInterval = keepaliveOptions && keepaliveOptions.interval
+      ? keepaliveOptions.interval : 45 * 1000;
 
     // Fallback in case no keepalive uri was found.
     if (keepaliveUri === '') {
       const systemPaths = getOptions('system.paths');
-
-      keepaliveUri = (systemPaths ? systemPaths.root + '/index.php' : window.location.pathname) + '?option=com_ajax&format=json';
+      const pathRoot = systemPaths ? `${systemPaths.root}/index.php` : window.location.pathname;
+      keepaliveUri = `${pathRoot}?option=com_ajax&format=json`;
     }
 
     window.setInterval(() => {
-      Request({
+      request({
         url: keepaliveUri,
         onSuccess: () => { },
-        onError: () => { }
+        onError: () => { },
       });
     }, keepaliveInterval);
   });
