e798db4db937081b25c105ac90f5588532baa51b
astridx <info@astrid-guenther.de>
2018-03-15 22:28:41 +0100

    iifw

--- media/com_newsfeeds/js/admin-field-modal.es6.js
+++ media/com_newsfeeds/js/admin-field-modal.es6.js
@@ -2,13 +2,18 @@
  * @copyright  Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-document.addEventListener('DOMContentLoaded', () => {
-  const inputGroupAppendId = document.getElementById('input-group-append-id');
-  if (inputGroupAppendId) {
-    const newsfeedId = inputGroupAppendId.getAttribute('data-newsfeed-id');
+(() => {
+  'use strict';
 
-    window[`jSelectNewsfeed_${newsfeedId}`] = (id, title, object) => {
-      window.processModalSelect('Newsfeed', newsfeedId, id, title, '', object);
-    };
-  }
-});
+  document.addEventListener('DOMContentLoaded', () => {
+    const inputGroupAppendId = document.getElementById('input-group-append-id');
+    if (inputGroupAppendId) {
+      const newsfeedId = inputGroupAppendId.getAttribute('data-newsfeed-id');
+
+      window[`jSelectNewsfeed_${newsfeedId}`] = (id, title, object) => {
+        window.processModalSelect('Newsfeed', newsfeedId, id, title, '', object);
+      };
+    }
+  });
+})();
+
--- media/com_newsfeeds/js/admin-field-modal.js
+++ media/com_newsfeeds/js/admin-field-modal.js
@@ -7,13 +7,17 @@
  * @copyright  Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-document.addEventListener('DOMContentLoaded', function () {
-  var inputGroupAppendId = document.getElementById('input-group-append-id');
-  if (inputGroupAppendId) {
-    var newsfeedId = inputGroupAppendId.getAttribute('data-newsfeed-id');
+(function () {
+  'use strict';
 
-    window['jSelectNewsfeed_' + newsfeedId] = function (id, title, object) {
-      window.processModalSelect('Newsfeed', newsfeedId, id, title, '', object);
-    };
-  }
-});
+  document.addEventListener('DOMContentLoaded', function () {
+    var inputGroupAppendId = document.getElementById('input-group-append-id');
+    if (inputGroupAppendId) {
+      var newsfeedId = inputGroupAppendId.getAttribute('data-newsfeed-id');
+
+      window['jSelectNewsfeed_' + newsfeedId] = function (id, title, object) {
+        window.processModalSelect('Newsfeed', newsfeedId, id, title, '', object);
+      };
+    }
+  });
+})();
