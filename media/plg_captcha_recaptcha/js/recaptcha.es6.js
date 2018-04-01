a6bbd7ada672e2ac6b8340604cc4bc362e339d50
Kasun Vithanage <alankasun@gmail.com>
2018-04-01 17:36:50 +0530

    converted to es6

--- media/plg_captcha_recaptcha/js/recaptcha.es6.js
+++ media/plg_captcha_recaptcha/js/recaptcha.es6.js
@@ -1,23 +1,18 @@
 /**
-* PLEASE DO NOT MODIFY THIS FILE. WORK ON THE ES6 VERSION.
-* OTHERWISE YOUR CHANGES WILL BE REPLACED ON THE NEXT BUILD.
-**/
-
-/**
  * @package     Joomla.JavaScript
  * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-window.JoomlaInitReCaptcha2 = function () {
+window.JoomlaInitReCaptcha2 = () => {
   'use strict';
 
-  var items = document.getElementsByClassName('g-recaptcha');
-  items.forEach(function (item) {
-    var options = item.dataset ? item.dataset : {
+  const items = document.getElementsByClassName('g-recaptcha');
+  items.forEach((item) => {
+    const options = item.dataset ? item.dataset : {
       sitekey: item.getAttribute('data-sitekey'),
       theme: item.getAttribute('data-theme'),
-      size: item.getAttribute('data-size')
+      size: item.getAttribute('data-size'),
     };
 
     /* global grecaptcha */
